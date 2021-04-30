7a62f09c8912d2c41d75f14a6ef5ac40f029ad34
chris-jan-trapp <christian.trapp@gbv.de>
2021-04-30 10:42:52 +0200

    Added Pipfile and real WFS requests.

--- src/server/external_storage.py
+++ src/server/external_storage.py
@@ -2,14 +2,48 @@ import json
 from datetime import datetime as d
 import settings
 import hashlib
+import xml.etree.ElementTree as ET
+import requests
 
 """This is modelled after https://docs.easydb.de/en/technical/plugins/
 section "Example (Server Callback)
 """
 
+TRANSACTION_ATTRIBUTES = {"version": "1.1.0",
+                          "service": "WFS",
+                          "xmlns": "http://esx-80.gbv.de:8080/geoserver/gbv",
+                          "xmlns:gbv": "gbv",
+                          "xmlns:gml": "http://www.opengis.net/gml",
+                          "xmlns:ogc": "http://www.opengis.net/ogc",
+                          "xmlns:wfs": "http://www.opengis.net/wfs",
+                          "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+                          "xsi:schemaLocation": """http://esx-80.gbv.de:8080/geoserver/
+                                                   http://www.opengis.net/wfs
+                                                   ../wfs/1.1.0/WFS.xsd"""
+                          }
+RESPONSE_NAMESPACE = {"wfs": "http://www.opengis.net/wfs",
+                      "ogc":"http://www.opengis.net/ogc"
+                      }
+GEO_SERVER_URL = "http://esx-80.gbv.de:8080/geoserver/wfs"
+
 
 def easydb_server_start(easydb_context):
     easydb_context.register_callback('db_pre_update', {'callback': 'dump_to_disk'})
+
+
+def create_transaction(feature_type, feature):
+    transaction = ET.Element("wfs:Transaction", **TRANSACTION_ATTRIBUTES)
+    insert = ET.SubElement(transaction, "wfs:Insert")
+    to_insert = ET.SubElement(insert, ":".join((TRANSACTION_ATTRIBUTES["xmlns:gbv"], feature_type)))
+    text = ET.SubElement(to_insert, 'text')
+    text.text = feature["text"]
+    found_at = ET.SubElement(to_insert)
+    point_property_type = ET.SubElement(found_at, 'gml:PointPropertyType', srsName="EPSG:4326")
+    pos = ET.SubElement(point_property_type, 'gml:pos')
+    coordinates = feature['location']['mapPosition']['position']
+    pos.text = str(coordinates['lat']) + ' ' + str(coordinates['lon'])
+
+    return ET.tostring(transaction)
 
 
 def dump_to_disk(easydb_context, easydb_info):
@@ -23,11 +57,8 @@ def dump_to_disk(easydb_context, easydb_info):
         index = payload.index(relevant_object)
         unpacked = relevant_object[settings.OBJECT_TYPE]
 
-        geometry = unpacked[settings.GEOMETRY]
-        attributes = dict([(attribute, unpacked.get(attribute, None)) for attribute in settings.ATTRIBUTES])
-        hash = pseudo_wfs({"geometry": geometry,
-                           "attributes": attributes})
-        payload[index][settings.OBJECT_TYPE][settings.RETURN] = hash
+        id = wfs(settings.OBJECT_TYPE, unpacked)
+        payload[index][settings.OBJECT_TYPE][settings.RETURN] = id
     return payload
 
 
@@ -45,3 +76,15 @@ def pseudo_wfs(feature):
     with open('/var/tmp/plugin.json', 'w') as tmp:
         json.dump(store, tmp, indent=2)
     return hash
+
+
+def wfs(feature_type, feature):
+    data = create_transaction(feature_type, feature)
+    response = requests.post(GEO_SERVER_URL,
+                             data=data,
+                             headers={"Content-type": "text/xml"})
+
+    if response.status_code == 200:
+        transaction_result = ET.fromstring(response.content)
+        feature_id = transaction_result.find("**/ogc:FeatureId", RESPONSE_NAMESPACE)
+        return feature_id.get('fid')
