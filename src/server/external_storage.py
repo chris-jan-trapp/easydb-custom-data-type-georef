5019bc59141bbc955ff7d0ec512e46a827c55d33
chris-jan-trapp <christian.trapp@gbv.de>
2021-05-27 10:23:49 +0200

    improved sql statement

--- src/server/external_storage.py
+++ src/server/external_storage.py
@@ -102,7 +102,7 @@ def wfs(feature_type, feature, method):
 
 
 def get_wfs_id(object_name, edb_id, context):
-    sql = "select from " + object_name + ' where "id:pkey"=' + str(edb_id) + ";"
+    sql = "select feature_id from " + object_name + ' where "id:pkey"=' + str(edb_id) + ";"
     logging.debug(sql)
     db_cursor = context.get_db_cursor()
     db_cursor.execute(sql)
