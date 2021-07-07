715aec6652e87b6e9b7d2e1d15a6aa18a3d652e4
chris-jan-trapp <christian.trapp@gbv.de>
2021-07-07 16:52:41 +0200

    easydb_info['data'] seems to be a list that json can't serialize. So I throw the whole easydb_info at the servicer.

--- src/server/external_storage.py
+++ src/server/external_storage.py
@@ -42,9 +42,8 @@ def easydb_server_start(easydb_context):
 
 def redirect_to_servicer(easydb_context, easydb_info):
     session = easydb_context.get_session()
-    data = easydb_info['data']
-    logging.info("\n".join(["Redirecting:", str(session), str(data)]))
-    response = requests.post("http://" + SERVICER_URL + "/dump", json={'session': session, data: data})
+    logging.info("\n".join(["Redirecting:", str(session), str(easydb_info)]))
+    response = requests.post("http://" + SERVICER_URL + "/dump", json={'session': session, "info": easydb_info})
     logging.info("Servicer says: " + str(response.content))
     return response.json()
 
