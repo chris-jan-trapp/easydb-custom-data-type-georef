6e703eaeeee7980178398242e609306e98db8012
chris-jan-trapp <christian.trapp@gbv.de>
2021-08-30 14:45:29 +0200

    Switched to post_update.

--- src/server/external_storage.py
+++ src/server/external_storage.py
@@ -13,7 +13,7 @@ SERVICER_URL = "servicer:5000"
 
 def easydb_server_start(easydb_context):
     # easydb_context.register_callback('db_pre_update', {'callback': 'dump_to_wfs'})
-    easydb_context.register_callback('db_pre_update', {'callback': 'redirect_to_servicer'})
+    # easydb_context.register_callback('db_pre_update', {'callback': 'redirect_to_servicer'})
     easydb_context.register_callback('db_post_update_one', {'callback': 'catch_post_update'})
 
     logging.basicConfig(filename="/var/tmp/plugin.log", level=logging.DEBUG)
@@ -23,7 +23,7 @@ def easydb_server_start(easydb_context):
 def redirect_to_servicer(easydb_context, easydb_info):
     session = easydb_context.get_session()
     logging.info("\n".join(["Redirecting:", str(session), str(easydb_info)]))
-    response = requests.post("http://" + SERVICER_URL + "/dump",
+    response = requests.post("http://" + SERVICER_URL + "/pre-update",
                              json={'session': session, "info": easydb_info},
                              headers={'Content-type': 'application/json'})
     logging.info("Servicer says: " + str(response.content))
